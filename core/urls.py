a7a7ee41b08d80aafb1b421bed0584504af5e635
aftabkhan07 <khanaftaba1@gmail.com>
2023-01-15 18:42:22 +0530

    Post Feed completed

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('logout', views.logout, name = 'logout'),
     path('upload', views.upload, name = 'upload'),
     path('like-post', views.like_post, name = 'like-post'),
+    path('profile/<str:pk>', views.profile, name = 'profile'),
+    path('follow', views.follow, name = 'follow'),
 ]
 
 
